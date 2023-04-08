8b2ed3dc0a3ddf39c4e8c96da2f2badfd3797ee3
JP <jplmarotinho22@gmail.com>
2023-04-08 11:44:57 -0300

    feat: :sparkles: Interface e validação de formulários usando zod

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,7 @@
 import { Play } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
 
 import {
   CountdownContainer,
@@ -10,21 +13,55 @@ import {
   TaskInput,
 } from './styles'
 
+// controlled ou uncontrolled
+// controlled: monitora o input e atualiza o state conforme o usuário digita
+
+/* quando setamos um state o react renderiza a página novamente, o uncontrolled
+é melhor para performance em interfaces mais complexas */
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa!'),
+  minutesAmount: zod.number().min(5).max(60),
+})
+
+interface NewCycleFormData {
+  task: string
+  minutesAmount: number
+}
+
 export function Home() {
+  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  })
+
+  function handleCreateNewCycle(data: any) {
+    console.log(data)
+  }
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
+  /* register retorna os métodos utilizados para trabalhar com forms no js
+  Ex: onChange, onSubmit, onFocus, required, etc. Por isso o spread operator */
+
   return (
     <HomeContainer>
-      <form>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
             list="task-suggestions"
             placeholder="Dê um nome para seu projeto"
             id="task"
+            {...register('task')}
           />
 
           <datalist id="task-suggestions">
-            <option value="Projeto 1"></option>
-            <option value="Projeto 2"></option>
+            <option value="Estudar"></option>
+            <option value="Revisar useContext"></option>
           </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
@@ -33,8 +70,7 @@ export function Home() {
             type="number"
             id="minutesAmount"
             step={5}
-            min={5}
-            max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
 
           <span>minutos.</span>
@@ -48,7 +84,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton disabled type="submit">
+        <StartCountdownButton disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
           Começar
         </StartCountdownButton>
